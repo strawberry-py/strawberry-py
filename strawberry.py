1d4d81dd0f8fda881b67dfefce5afcf16843252a
Vojtěch Olej <Vojtech.Olej@logex.com>
2024-04-11 17:34:24 +0200

    [Strawberry] Re-write bot as class

--- strawberry.py
+++ strawberry.py
@@ -1,16 +1,16 @@
+from __future__ import annotations
+
 import asyncio
 import os
 import platform
 import sys
-import traceback
 from pathlib import Path
 from typing import Dict
 
 import discord
-import sqlalchemy
-from discord.ext import commands
 
 from pie import exceptions
+from pie.bot import Strawberry
 from pie.cli import COLOR
 
 # Setup checks
@@ -58,7 +58,7 @@ def print_versions():
     ):
         print(
             "- "
-            f"{repo_name.ljust(longest_repo_name)} "
+            f"{repository_name.ljust(longest_repo_name)} "
             f"{color}{repository_version}{COLOR.none}"
         )
 
@@ -101,191 +101,8 @@ os.chdir(root_path)
 del root_path
 
 
-# Database
-
-
-from pie import database
-
-database.init_core()
-database.init_modules()
-
-
-# Load or create config object
-
-
-config = database.config.Config.get()
-
-
-# Setup discord
-
-intents = discord.Intents.all()
-
-from pie import utils
-from pie.help import Help
-
-bot = commands.Bot(
-    allowed_mentions=discord.AllowedMentions(roles=False, everyone=False, users=True),
-    command_prefix=config.prefix,
-    help_command=Help(),
-    intents=intents,
-)
-# This is required to make the 'bot' object hashable by ring's LRU cache
-# See pie/acl/__init__.py:map_member_to_ACLevel()
-bot.__ring_key__ = lambda: "bot"
-
-
-# Setup logging
-
-from pie import logger
-
-bot_log = logger.Bot.logger(bot)
-guild_log = logger.Guild.logger(bot)
-
-
-# Setup listeners
-
-already_loaded: bool = False
-
-
-async def update_app_info(bot: commands.Bot):
-    # Update bot information
-    app: discord.AppInfo = await bot.application_info()
-    if app.team:
-        bot.owner_ids = {m.id for m in app.team.members}
-    else:
-        bot.owner_ids = {app.owner.id}
-
-
-@bot.event
-async def on_ready():
-    """This is run on login and on reconnect."""
-    global already_loaded
-
-    # Update information about user's owners
-    await update_app_info(bot)
-
-    # If the status is set to "auto", let the loop in Admin module take care of it
-    status = "invisible" if config.status == "auto" else config.status
-    await utils.discord.update_presence(bot, status=status)
-
-    await bot.tree.sync()
-
-    if already_loaded:
-        await bot_log.info(None, None, "Reconnected")
-    else:
-        print(
-            "\n"
-            "     (     \n"
-            "  (   )  ) \n"
-            "   )  ( )  \n"
-            "   .....   \n"
-            ".:::::::::.\n"
-            "~\\_______/~\n"
-        )
-        await bot_log.critical(None, None, "The pie is ready.")
-        already_loaded = True
-
-
-async def handle_error(error: Exception, source: str):
-    # Make sure we rollback the database session if we encounter an error
-    if isinstance(error, sqlalchemy.exc.SQLAlchemyError):
-        database.session.rollback()
-        database.session.commit()
-        await bot_log.critical(
-            None,
-            None,
-            "strawberry.py database session rolled back. The bubbled-up cause is:\n"
-            + "\n".join([f"| {line}" for line in str(error).split("\n")]),
-        )
-    else:
-        await bot_log.critical(
-            None,
-            None,
-            (
-                "Uncaught error bubbled-up.\n"
-                + (f"Source: {source}.\n" if source else "")
-                + "Traceback: \n"
-                + str(error)
-                + "\n"
-                + "\n".join(traceback.format_tb(error.__traceback__))
-            ),
-        )
-
-
-async def on_error(event, *args, **kwargs):
-    error_type, error, tb = sys.exc_info()
-
-    await handle_error(error)
-
-
-commands.Bot.on_error = on_error
-
-
-async def on_itx_error(self, itx: discord.Interaction, error: Exception) -> None:
-    source = itx.command.qualified_name if itx.command else None
-    await handle_error(error, source)
-
-
-discord.ui.Modal.on_error = on_itx_error
-discord.ui.View.on_error = on_itx_error
-discord.app_commands.Command.on_error = on_itx_error
-discord.app_commands.Group.on_error = on_itx_error
-discord.app_commands.CommandTree.on_error = on_itx_error
-
-
-from modules.base.admin.database import BaseAdminModule
-
-
-async def load_modules():
-    modules = (
-        "base.acl",
-        "base.admin",
-        "base.baseinfo",
-        "base.errors",
-        "base.language",
-        "base.logging",
-    )
-    db_modules = BaseAdminModule.get_all()
-    db_module_names = [m.name for m in db_modules]
-
-    for module in modules:
-        if module in db_module_names:
-            # This module is managed by database
-            continue
-        await bot.load_extension(f"modules.{module}.module")
-        print(
-            f"Module {COLOR.green}{module}{COLOR.none} loaded.",
-            file=sys.stdout,
-        )  # noqa: T001
-
-    for module in db_modules:
-        if not module.enabled:
-            print(
-                f"Module {COLOR.yellow}{module.name}{COLOR.none} found, but is disabled.",
-                file=sys.stdout,
-            )  # noqa: T001
-            continue
-        try:
-            await bot.load_extension(f"modules.{module.name}.module")
-        except (ImportError, ModuleNotFoundError, commands.ExtensionNotFound):
-            print(
-                f"Module {COLOR.red}{module.name}{COLOR.none} not found.",
-                file=sys.stdout,
-            )  # noqa: T001
-            continue
-        print(
-            f"Module {COLOR.green}{module.name}{COLOR.none} loaded.",
-            file=sys.stdout,
-        )  # noqa: T001
-
-    for command in bot.walk_commands():
-        if type(command) is not commands.Group:
-            command.ignore_extra = False
-
-
 async def main():
-    await load_modules()
-    await bot.start(os.getenv("TOKEN"))
+    await Strawberry().start()
 
 
 asyncio.run(main())
